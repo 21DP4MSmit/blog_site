e97e9dbd69aef53886166febc6713ba19a4c476c
21DP4MSmit <103188916+21DP4MSmit@users.noreply.github.com>
2024-06-07 01:49:54 +0300

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,32 +1,31 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>{{ !empty($meta_title) ? $meta_title : '' }}</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="shortcut icon" href="favicon.png">
-	@if(!empty($meta_keyword))
-	<meta name="keyword" content="{{ $meta_keyword }}" />
-	@endif
-	@if(!empty($meta_description))
-	<meta name="description" content="{{ $meta_description }}" />
-	@endif
+    <meta charset="utf-8">
+    <title>{{ !empty($meta_title) ? $meta_title : '' }}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="favicon.png">
+    @if(!empty($meta_keyword))
+    <meta name="keyword" content="{{ $meta_keyword }}" />
+    @endif
+    @if(!empty($meta_description))
+    <meta name="description" content="{{ $meta_description }}" />
+    @endif
 
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 
+    <link rel="stylesheet" href="{{ url('css/icomoon/style.css') }}">
+    <link rel="stylesheet" href="{{ url('css/flaticon/font/flaticon.css') }}">
 
-	<link rel="stylesheet" href="{{ url('front/fonts/icomoon/style.css') }}">
-	<link rel="stylesheet" href="{{ url('front/fonts/flaticon/font/flaticon.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
-
-	<link rel="stylesheet" href="/assets/css/tiny-slider.css">
-	<link rel="stylesheet" href="/assets/css/aos.css">
-	<link rel="stylesheet" href="/assets/css/glightbox.min.css">
-	<link rel="stylesheet" href="/assets/css/style2.css">
-	<link rel="stylesheet" href="/assets/css/flatpickr.min.css">
+    <link rel="stylesheet" href="{{ url('css/tiny-slider.css') }}">
+    <link rel="stylesheet" href="{{ url('css/aos.css') }}">
+    <link rel="stylesheet" href="{{ url('css/glightbox.min.css') }}">
+    <link rel="stylesheet" href="{{ url('css/style2.css') }}">
+    <link rel="stylesheet" href="{{ url('css/flatpickr.min.css') }}">
 
     @yield('style')
 </head>
@@ -38,25 +37,23 @@
 
 @include('layouts.footer')
 
-
 <!-- Preloader -->
 <div id="overlayer"></div>
     <div class="loader">
-    	<div class="spinner-border text-primary" role="status">
-    		<span class="visually-hidden">Loading...</span>
-    	</div>
+        <div class="spinner-border text-primary" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
     </div>
 
-
-    <script src="/assets/js/bootstrap.bundle.min.js"></script>
-    <script src="/assets/js/tiny-slider.js"></script>
-    <script src="/assets/js/flatpickr.min.js"></script>
-    <script src="/assets/js/aos.js"></script>
-    <script src="/assets/js/glightbox.min.js"></script>
-    <script src="/assets/js/navbar.js"></script>
-    <script src="/assets/js/counter.js"></script>
-    <script src="/assets/js/custom.js"></script>
-	<script src="/assets/js/main.js"></script>
+    <script src="{{ url('js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ url('js/tiny-slider.js') }}"></script>
+    <script src="{{ url('js/flatpickr.min.js') }}"></script>
+    <script src="{{ url('js/aos.js') }}"></script>
+    <script src="{{ url('js/glightbox.min.js') }}"></script>
+    <script src="{{ url('js/navbar.js') }}"></script>
+    <script src="{{ url('js/counter.js') }}"></script>
+    <script src="{{ url('js/custom.js') }}"></script>
+    <script src="{{ url('js/main.js') }}"></script>
     @yield('script')
 </body>
 </html>
