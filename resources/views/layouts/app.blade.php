b9cdb43d7cd2812be0a1a1d66a210715e55dc455
21DP4MSmit <103188916+21DP4MSmit@users.noreply.github.com>
2024-06-07 01:31:40 +0300

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,16 +23,16 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 
 	<!-- <link rel="stylesheet" href="/assets/css/tiny-slider.css"> -->
-	@include('resources\css\tiny-slider.blade.php')
+	@include('resources/css/tiny-slider.blade.php')
 	<!-- <link rel="stylesheet" href="/assets/css/aos.css"> -->
-	@include('resources\css\aos.blade.php')
+	@include('resources/css/aos.blade.php')
 	<!-- <link rel="stylesheet" href="/assets/css/glightbox.min.css"> -->
-	@include('resources\css\glightbox.min.blade.php')
+	@include('resources/css/glightbox.min.blade.php')
 	<!-- <link rel="stylesheet" href="/assets/css/style2.css"> -->
-	@include('resources\css\style.blade.php')
+	@include('resources/css/style.blade.php')
 
 	<!-- <link rel="stylesheet" href="/assets/css/flatpickr.min.css"> -->
-	@include('resources\css\flatpickr.min.blade.php')
+	@include('resources/css/flatpickr.min.blade.php')
 
     @yield('style')
 </head>
@@ -55,21 +55,21 @@
 
 
     <!-- <script src="/assets/js/bootstrap.bundle.min.js"></script> -->
-	@include('resources\js\bootstrap.bundle.min.blade.php')
+	@include('resources/js/bootstrap.bundle.min.blade.php')
     <!-- <script src="/assets/js/tiny-slider.js"></script> -->
-	@include('resources\js\tiny-slider.blade.php')
+	@include('resources/js/tiny-slider.blade.php')
     <!-- <script src="/assets/js/flatpickr.min.js"></script> -->
-	@include('resources\js\flatpickr.min.blade.php')
+	@include('resources/js/flatpickr.min.blade.php')
     <!-- <script src="/assets/js/aos.js"></script> -->
-	@include('resources\js\aos.blade.php')
+	@include('resources/js/aos.blade.php')
     <!-- <script src="/assets/js/glightbox.min.js"></script> -->
-	@include('resources\js\glightbox.min.blade.php')
+	@include('resources/js/glightbox.min.blade.php')
     <!-- <script src="/assets/js/navbar.js"></script> -->
-	@include('resources\js\navbar.blade.php')
+	@include('resources/js/navbar.blade.php')
     <!-- <script src="/assets/js/counter.js"></script> -->
-	@include('resources\js\counter.blade.php')
+	@include('resources/js/counter.blade.php')
     <!-- <script src="/assets/js/custom.js"></script> -->
-	@include('resources\js\custom.blade.php')
+	@include('resources/js/custom.blade.php')
 	<script src="/assets/js/main.js"></script>
     @yield('script')
 </body>
